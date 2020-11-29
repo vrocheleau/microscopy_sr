Fixed gradient accumulation error

main.py
@@ -16,7 +16,6 @@ from losses.esrgan import PerceptualLoss
 warnings.filterwarnings("ignore", module=".*aicsimageio")
 
 ex = Experiment()
-ex.observers.append(FileStorageObserver('sacred_runs'))
 
 @ex.config
 def conf():
@@ -36,6 +35,7 @@ def conf():
     is_gan = False
     psnr_oriented = False
     state_dict = None
+    seed = 0
 
 @ex.capture
 def opt_sch_ABPN(model, lr_step, lr):
@@ -68,12 +68,15 @@ def losses(device, name):
 
 @ex.capture
 def get_exp_id(_run):
-    return _run._id
+    return _run._id, _run.observers[0].basedir
+
 
 @ex.automain
 def main(name, scale_factor, multi_gpu, batch_size, patch_size, chanels, epochs, val_intervals, preload, is_gan, psnr_oriented, state_dict, fold, gpu_id):
 
     torch.backends.cudnn.benchmark = True
+
+    exp_id, basedir = get_exp_id()
 
     if torch.cuda.is_available():
         device = 'cuda:{}'.format(gpu_id)
@@ -123,10 +126,8 @@ def main(name, scale_factor, multi_gpu, batch_size, patch_size, chanels, epochs,
     else:
         best_state_dict = model.state_dict()
 
-    exp_id = get_exp_id()
-    exp_dir = os.path.join('sacred_runs/', exp_id)
-    name = os.path.join(exp_dir, '{}_{}x.pth'.format(name.upper(), scale_factor))
-    torch.save(best_state_dict, name)
+    exp_dir = os.path.join(basedir, exp_id)
+    torch.save(best_state_dict, os.path.join(exp_dir, '{}_{}x.pth'.format(name.upper(), scale_factor)))
 
     # Test phase
     save_dir = 'out/{}/{}x'.format(name.upper(), scale_factor)

train.py
@@ -49,6 +49,7 @@ def train_gan(generator: nn.Module, discriminator: nn.Module, train_ds, val_ds, 
     val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
 
     best_val_loss = float('inf')
+
     if isinstance(generator, torch.nn.DataParallel):
         best_state_dict = deepcopy(generator.module.state_dict())
     else:
@@ -193,10 +194,12 @@ def train(model, train_ds, val_ds, epochs, val_intervals,batch_size, opt_sch_cal
 
     writer = SummaryWriter()
 
+    mini_batch = 0
+
     for epoch in range(epochs):
 
         model.train()
-        optimizer.zero_grad()
+
 
         for HR, LR in tqdm(train_loader, ncols=100, desc='[{}/{}]Training'.format(epoch, epochs)):
             HR, LR = HR.to(device), LR.to(device)
@@ -204,25 +207,26 @@ def train(model, train_ds, val_ds, epochs, val_intervals,batch_size, opt_sch_cal
             out = model(LR)
             loss = loss_object(out, HR)
 
+            optimizer.zero_grad()
             loss.backward()
+            writer.add_scalar('Loss_mini/train_g', loss.item(), mini_batch)
 
             optimizer.step()
+
+            mini_batch += 1
 
         if scheduler is not None:
             scheduler.step()
 
         # Eval model
         if epoch != 0 and epoch % val_intervals == 0:
-
             val_metrics = eval(model, val_loader, loss_object, test=False, device=device)
-
             if val_metrics['losses'].mean() <= best_val_loss:
                 best_val_loss = val_metrics['losses'].mean()
                 if isinstance(model, torch.nn.DataParallel):
                     best_state_dict = deepcopy(model.module.state_dict())
                 else:
                     best_state_dict = deepcopy(model.state_dict())
-
                 if checkpoint_path is not None:
                     print("Checkpoint saving state dict")
                     torch.save(best_state_dict, checkpoint_path)